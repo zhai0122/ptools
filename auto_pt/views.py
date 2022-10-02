b7c4ee746dafd27fd0e4e1099fa6f85d9786bc1b
ngfchl <ngfchl@126.com>
2022-10-02 08:41:12 +0800

    根据tracker地址返回站点名称，并实现筛选筛选功能

--- auto_pt/views.py
+++ auto_pt/views.py
@@ -97,7 +97,7 @@ def get_downloading(request):
     id = request.GET.get('id')
     print(id)
     downloader = Downloader.objects.filter(id=id).first()
-    tracker_filters = Site.objects.all().values('id', 'name', 'tracker')
+    tracker_list = Site.objects.all().values('id', 'name', 'tracker')
     qb_client = qbittorrentapi.Client(
         host=downloader.host,
         port=downloader.port,
@@ -163,7 +163,7 @@ def get_downloading(request):
         print(len(torrents))
         main_data['torrents'] = torrents
         # print(tracker_filters)
-        main_data['tracker_filters'] = list(tracker_filters)
+        main_data['tracker_list'] = list(tracker_list)
         # return JsonResponse(CommonResponse.success(data=torrents).to_dict(), safe=False)
         return JsonResponse(CommonResponse.success(data=main_data).to_dict(), safe=False)
     except Exception as e:
