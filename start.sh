d2cb2d6d070d98e936c7331a11d9ea9932cfe6f6
ngfchl <ngfchl@126.com>
2022-10-05 00:37:44 +0800

    修复启动脚本错误

--- start.sh
+++ start.sh
@@ -2,18 +2,18 @@
 # 升级pip到最新
 python -m pip install --upgrade pip &&
   # 写入U2的HOSTS
-  echo 172.64.153.252 u2.dmhy.org >> /etc/hosts &&
+  echo 172.64.153.252 u2.dmhy.org >>/etc/hosts &&
   CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
   echo "-- First container startup --"
   # 此处插入你要执行的命令或者脚本文件
-  git config --global init.defaultBranch master &&
     git init &&
     git remote add origin https://gitee.com/ngfchl/ptools &&
     # 设置拉取最新文件并覆盖
     git config pull.ff only &&
     git pull origin $DEV &&
-  ls -l && pwd
+    ls -l && pwd &&
+    cp /var/www/html/ptools/start.sh /var/www/html/start.sh
   #    git branch --set-upstream-to=origin/master master &&
   pip install -r requirements.txt
   python manage.py makemigrations
@@ -30,9 +30,15 @@ else
   echo "-- Not first container startup --"
 fi
 
-if [ $DEV == 'dev']; then
-      echo '您当前处于开发者测试版本，请注意备份数据'
+echo $DEV
+
+if [[ $DEV == dev ]]; then
+  echo "您当前处于开发版本，请注意备份文件"
+else
+  echo "您当前处于稳定版本"
+fi
+
 pip install -r requirements.txt &&
   python manage.py makemigrations &&
   python manage.py migrate &&
-  python manage.py runserver 0.0.0.0:$DJANGO_WEB_PORT
+  python manage.py runserver $DJANGO_WEB_PORT
