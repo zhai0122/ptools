93051a9dc48ce51a3de6a40f6d62a501b8d821a7
ngfchl <ngfchl@126.com>
2022-10-07 21:29:57 +0800

    日志重复了，取消启动文件单独写入的日志

--- start.sh
+++ start.sh
@@ -37,4 +37,4 @@ fi
 echo "写入U2 hosts"
 echo 172.64.153.252 u2.dmhy.org >>/etc/hosts
 echo "启动服务"
-python manage.py runserver 0.0.0.0:$DJANGO_WEB_PORT >>/var/www/html/ptools/db/$(date "+%Y%m%d").log 2>&1
+python manage.py runserver 0.0.0.0:$DJANGO_WEB_PORT
