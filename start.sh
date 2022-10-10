236dfbfefaaa3a4767414f359d2cd922f2474973
ngfchl <ngfchl@126.com>
2022-10-10 17:01:10 +0800

    优化启动与更新脚本

--- start.sh
+++ start.sh
@@ -7,32 +7,33 @@ else
 fi
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
   echo "-- First container startup --"
-    # 此处插入你要执行的命令或者脚本文件
-    echo "升级pip"
-    python -m pip install --upgrade pip
-    echo "拉取PTools最新代码"
-    # 设置拉取最新文件并覆盖
-    git config pull.ff only
-    git pull origin $DEV
-    echo "列出代码文件信息"
-    ls -l
-    echo "安装pip依赖"
-    pip install -r requirements.txt
-    echo "初始化数据库"
-    python manage.py makemigrations
-    python manage.py migrate
-    python manage.py loaddata pt.json
-    touch $CONTAINER_ALREADY_STARTED
-    echo "创建超级用户"
-    DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME
-    DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL
-    DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD
-    python manage.py createsuperuser --noinput
-    echo "初始化完成"
+  # 此处插入你要执行的命令或者脚本文件
+  echo "升级pip"
+  python -m pip install --upgrade pip
+  echo "拉取PTools最新代码"
+  # 设置拉取最新文件并覆盖
+  git config pull.ff only
+  git pull
+  git pull origin $DEV
+  echo "列出代码文件信息"
+  ls -l
+  echo "安装pip依赖"
+  pip install -r requirements.txt
+  echo "初始化数据库"
+  python manage.py makemigrations
+  python manage.py migrate
+  python manage.py loaddata pt.json
+  touch $CONTAINER_ALREADY_STARTED
+  echo "创建超级用户"
+  DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME
+  DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL
+  DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD
+  python manage.py createsuperuser --noinput
+  echo "初始化完成"
 else
   echo "-- Not first container startup --"
 fi
- echo "写入U2 hosts"
- echo 172.64.153.252 u2.dmhy.org >>/etc/hosts
+echo "写入U2 hosts"
+echo 172.64.153.252 u2.dmhy.org >>/etc/hosts
 echo "启动服务"
-  python manage.py runserver 0.0.0.0:$DJANGO_WEB_PORT
+python manage.py runserver 0.0.0.0:$DJANGO_WEB_PORT
--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 # 先备份数据库文件再拉取更新 $(date "+%Y%m%d%H%M%S")当前时间年月日时分秒
-cp /var/www/html/ptools/db/db.sqlite3 /var/www/html/ptools/db/db.sqlite3-$(date "+%Y%m%d%H%M%S") &&
-  git pull origin $DEV &&
-  cp -f /var/www/html/ptools/start.sh /var/www/html/start.sh &&
-  pip install -r requirements.txt -U &&
-  python manage.py makemigrations &&
-  python manage.py migrate
+cp /var/www/html/ptools/db/db.sqlite3 /var/www/html/ptools/db/db.sqlite3-$(date "+%Y%m%d%H%M%S")
+git pull
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
