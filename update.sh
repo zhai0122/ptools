419e326977902a0d2b3342e7e49f248ab90f336e
ngfchl <ngfchl@126.com>
2022-10-10 23:03:36 +0800

    更新update.sh更新脚本

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 # 先备份数据库文件再拉取更新 $(date "+%Y%m%d%H%M%S")当前时间年月日时分秒
-cp /var/www/html/ptools/db/db.sqlite3 /var/www/html/ptools/db/db.sqlite3-$(date "+%Y%m%d%H%M%S")
+cp /ptools/db/db.sqlite3 /ptools/db/db.sqlite3-$(date "+%Y%m%d%H%M%S")
 git pull
 pip install -r requirements.txt
 python manage.py makemigrations
